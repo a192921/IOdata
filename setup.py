156bfabcfe1fa303b6fc604d95f33767581c7000
queena wu <ac192921@gmail.com>
2023-12-10 02:34:57 +0800

    setupV1

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 import sys
 from cx_Freeze import setup, Executable
 
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {
+    "packages": ["tkinter", "tkcalendar", "datetime","pyppeteer"],
+    "includes": ["tkinter", "numpy","os","asyncio"],
+    "include_msvcr": True
+    }
 
 # base="Win32GUI" should be used only for Windows GUI app
-# base = "Win32GUI" if sys.platform == "win32" else None
+base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
     name="IOdata",
     version="1.0",
     description="IOdata",
-    executables=[Executable("IOdata.py", icon="favicon.ico", base="Win32GUI")],
+    options={"build_exe": build_exe_options},
+    executables=[Executable("IOdata.py", icon="favicon.ico", base=base)],
+    # includes=["tkinter", "pandas", "sys", "tkcalendar", "numpy", "datetime","os", "asyncio", "pyppeteer"]
 )
